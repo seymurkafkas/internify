ae6158d2a4fa627fa0e4cd2ef4dadbd46b5f5a66
seymurkafkas <kafkas17@itu.edu.tr>
2020-11-29 16:00:27 +0000

    Add space before Internify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to
-          <a href="https://nextjs.org">Internify</a>
+          Welcome to <a href="https://nextjs.org">Internify</a>
         </h1>
 
         <p className={styles.description}>
