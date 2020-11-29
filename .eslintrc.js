09fb5742fac18b19c026f50c0dabb4467e14c880
seymurkafkas <kafkas17@itu.edu.tr>
2020-11-29 14:38:04 +0000

    Integrate Eslint with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,11 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -16,8 +21,25 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint", "import", "react-hooks", "prettier"],
   rules: {
     "react/react-in-jsx-scope": 0,
+    "@typescript-eslint/ban-types": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/no-empty-function": 0,
+    "@typescript-eslint/no-empty-interface": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "@typescript-eslint/no-unused-vars": 2,
+    "no-empty-pattern": 0,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".json", ".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
   },
 };
