034cbe3f8e6adeb31e097d127ee80fec0024580a
seymurkafkas <seymurkafkas@gmail.com>
2020-12-04 15:20:36 +0000

    Added email and password validation

---
+++ src/pages/api/index.ts
@@ -0,0 +1,25 @@
+/**
+ * @param {string} email- Email which will be checked
+ * @returns {boolean} -The email matches the email format
+ */
+export function isEmailValid(email: string) {
+  if (email === "") {
+    return false;
+  }
+  const mailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(mailformat)) {
+    return true;
+  }
+  return false;
+}
+/**
+ * @param {string} string1 -- The first string
+ * @param {string} string2 -- The second string
+ * @returns {boolean} -- The strings are equal
+ */
+export function isEqual(string1: string, string2: string) {
+  if (string1 === "" || string2 === "") {
+    return false;
+  }
+  return string1.localeCompare(string2) === 0;
+}
