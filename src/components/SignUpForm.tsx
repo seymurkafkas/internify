471c87ee50cc0843cd174844dbf65136777f0e85
seymurkafkas <seymurkafkas@gmail.com>
2020-12-10 18:23:48 +0000

    Added SignUpForm & small util fixes

---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import * as FormUtil from "../util/string";
+import * as Auth from "../services/auth";
+import {
+  Button,
+  Callout,
+  InputGroup,
+  Card,
+  Popover,
+  Elevation,
+  Position,
+  PopoverInteractionKind,
+  Intent,
+} from "@blueprintjs/core";
+import styles from "./SignUpForm.module.css";
+
+export default function SignUpForm() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [secondPassword, setSecondPassword] = React.useState("");
+  const emailIsValid = FormUtil.isEmailValid(email);
+  const passwordIsValid = FormUtil.areEqual(password, secondPassword);
+  const entriesAreValid = emailIsValid && passwordIsValid;
+  function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(event.target.value);
+  }
+
+  function handleSecondPasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setSecondPassword(event.target.value);
+  }
+
+  async function handleSubmit(event: React.MouseEvent) {
+    event.preventDefault();
+    (async () => {
+      try {
+        await Auth.createUser(email, password);
+        console.log("Success");
+      } catch (err) {
+        console.log("Unknown Error");
+      }
+    })();
+  }
+
+  return (
+    <Card elevation={Elevation.THREE} className={styles.logInBox}>
+      <InputGroup
+        disabled={false}
+        large={true}
+        placeholder="Enter your email address..."
+        small={false}
+        onChange={handleEmailChange}
+        value={email}
+        type={"text"}
+      />
+      <InputGroup
+        className={styles.inside}
+        disabled={false}
+        large={true}
+        placeholder="Enter your password..."
+        small={false}
+        onChange={handlePasswordChange}
+        value={password}
+        type={"password"}
+      />
+      <InputGroup
+        className={styles.inside}
+        disabled={false}
+        large={true}
+        placeholder="Enter your password..."
+        small={false}
+        value={secondPassword}
+        onChange={handleSecondPasswordChange}
+        type={"password"}
+      />
+      <Popover
+        className={styles.test}
+        minimal
+        interactionKind={PopoverInteractionKind.CLICK}
+        position={Position.RIGHT_TOP}>
+        <Button
+          intent={Intent.PRIMARY}
+          className={styles.inside}
+          minimal
+          icon="log-in"
+          disabled={!entriesAreValid}
+          onClick={handleSubmit}>
+          Register
+        </Button>
+        {entriesAreValid || (
+          <Callout
+            icon="error"
+            intent={Intent.WARNING}
+            title={`Invalid ${emailIsValid ? "Password" : passwordIsValid ? "Email" : "Email and Password"}`}>
+            {emailIsValid
+              ? "Please enter a valid password"
+              : passwordIsValid
+              ? "Please enter a valid email"
+              : "Please enter a valid email and password"}
+          </Callout>
+        )}
+      </Popover>
+    </Card>
+  );
+}
---
+++ src/pages/Register.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import SignUpForm from "../components/SignUpForm";
+export default function Register() {
+  return <SignUpForm />;
+}
