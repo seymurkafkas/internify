fc36cf456a16be6dcc2ce226cfff75152f5e8a2b
seymurkafkas <kafkas17@itu.edu.tr>
2020-12-25 11:41:56 +0000

    Student profile update & date component

--- src/components/EnrolledItem.tsx
+++ src/components/EnrolledItem.tsx
@@ -2,12 +2,27 @@ import React from "react";
 
 import { DateRangeInput } from "@blueprintjs/datetime";
 
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 export default function EnrolledItem() {
   return (
     <>
       <div>School</div>
       <DateRangeInput
-        formatDate={(date) => date.toLocaleString()}
+        formatDate={(date) => monthNames[date.getMonth()] + " " + date.getFullYear()}
         onChange={() => {}}
         parseDate={(str) => new Date(str)}
       />
