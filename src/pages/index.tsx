656a9dc57c568e815351a79acdc8c12a77660137
seymurkafkas <seymurkafkas@gmail.com>
2020-12-25 15:44:29 +0000

    Link should contain an a tag online

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,16 @@ export default function Home() {
   return (
     <div className="absolute w-full h-full bg-auth-background">
       <div className="absolute inset-1/2  h-2/6 w-1/6 justify-around items-center transform -translate-x-1/2 -translate-y-1/2  flex flex-row">
-        <Link href="/Register">
-          <Card elevation={Elevation.THREE}>
+        <Card elevation={Elevation.THREE}>
+          <Link href="/Register">
             <a>Register</a>
-          </Card>
-        </Link>
-        <Link href="/Login">
-          <Card elevation={Elevation.THREE}>
+          </Link>
+        </Card>
+        <Card elevation={Elevation.THREE}>
+          <Link href="/Login">
             <a>Login</a>
-          </Card>
-        </Link>
+          </Link>
+        </Card>
       </div>
     </div>
   );
