c225f7a913b1d37136dfad1063703e84083773bc
seymurkafkas <seymurkafkas@gmail.com>
2020-12-31 15:12:40 +0000

    Fixed navigation service wrappers

--- src/services/navigation/index.ts
+++ src/services/navigation/index.ts
@@ -1,37 +1,37 @@
 import { NextRouter } from "next/router";
 
-export function goToHome(router: NextRouter) {
-  router.push("/Home");
+export async function goToHome(router: NextRouter) {
+  await router.push("/Home");
 }
 
-export function goToIndex(router: NextRouter) {
-  router.push("/");
+export async function goToIndex(router: NextRouter) {
+  await router.push("/");
 }
 
-export function goToSearchPage(router: NextRouter) {
-  router.push("/Search");
+export async function goToSearchPage(router: NextRouter) {
+  await router.push("/Search");
 }
 
-export function goToMyApplicationsPage(router: NextRouter) {
-  router.push("/");
+export async function goToMyApplicationsPage(router: NextRouter) {
+  await router.push("/");
 }
 
-export function goToProfilePage(router: NextRouter) {
-  router.push("/Profile");
+export async function goToProfilePage(router: NextRouter) {
+  await router.push("/Profile");
 }
 
-export function goToProfilePageEmployer(router: NextRouter) {
-  router.push("/CompanyDetail");
+export async function goToProfilePageEmployer(router: NextRouter) {
+  await router.push("/CompanyDetail");
 }
 
-export function goToExplorePage(router: NextRouter) {
-  router.push("/");
+export async function goToExplorePage(router: NextRouter) {
+  await router.push("/");
 }
 
-export function goToPostAListingPage(router: NextRouter) {
-  router.push("/");
+export async function goToPostAListingPage(router: NextRouter) {
+  await router.push("/");
 }
 
-export function goToMyListingsPage(router: NextRouter) {
-  router.push("/");
+export async function goToMyListingsPage(router: NextRouter) {
+  await router.push("/");
 }
