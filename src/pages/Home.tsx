c94bfeca93918be4274573b76115735f5858339c
seymurkafkas <kafkas17@itu.edu.tr>
2021-01-01 19:46:01 +0000

    Merged home pages & redirect acc to user roles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import LayoutSignedInStudent from "../components/LayoutSignedInStudent";
+import LayoutSignedInEmployer from "../components/LayoutSignedInEmployer";
+import { useUser } from "../services/auth/userContext";
 
 export default function Login() {
-  return (
-    <LayoutSignedInStudent>
-      <div />
-    </LayoutSignedInStudent>
-  );
+  const user = useUser();
+  let content;
+  const typeOfUser = user?.user?.userType ?? null;
+  if (typeOfUser === "Student") {
+    content = (
+      <LayoutSignedInStudent>
+        <div />
+      </LayoutSignedInStudent>
+    );
+  } else if (typeOfUser === "Employer") {
+    content = (
+      <LayoutSignedInEmployer>
+        <div />
+      </LayoutSignedInEmployer>
+    );
+  } else {
+    content = null;
+  }
+  return content;
 }
