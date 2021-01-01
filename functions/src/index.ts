74d3e5b4f399bf57f7d1590aac19fc0a849e5efc
seymurkafkas <seymurkafkas@gmail.com>
2021-01-01 13:34:26 +0000

    Cloud function fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,10 +4,16 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export const setUserType = functions.https.onCall(async (data, context) => {
-  try {
-    await admin.auth().setCustomUserClaims(context.auth.uid, { userType: data });
-    return true;
-  } catch (err) {
-    console.log(err);
+  const userId = context?.auth?.uid ?? null;
+  if (userId) {
+    try {
+      await admin.auth().setCustomUserClaims(userId, { userType: data });
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  } else {
+    return false;
   }
 });
