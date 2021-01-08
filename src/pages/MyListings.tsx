0ac50518536889abce80bed26dc3877e05059a51
seymurkafkas <seymurkafkas@gmail.com>
2021-01-08 13:54:00 +0000

    Employer Data on Firestore & MyListings fix

--- src/pages/MyListings.tsx
+++ src/pages/MyListings.tsx
@@ -5,14 +5,42 @@ import SmallListingContainer from "../components/SmallListingContainer";
 // import * as db from "../services/firestore";
 
 export default function MyListingsPage() {
-  const [myListings] = React.useState([]);
+  const [myListings] = React.useState([
+    {
+      title: "Frontend developer",
+      location: "Istanbul, Turkey",
+      applicationCount: 32,
+      dateEnd: "3/7/2020",
+    },
+    {
+      title: "Backend Developer ",
+      location: "Istanbul, Turkey",
+      applicationCount: 32,
+      dateEnd: "3/7/2020",
+    },
+    {
+      title: "Perdeci",
+      location: "Istanbul, Turkey",
+      applicationCount: 32,
+      dateEnd: "3/7/2020",
+    },
+  ]);
 
   React.useEffect(() => {});
+
   return (
     <LayoutSignedInEmployer>
       <div className="flex flex-row justify-start">
-        {myListings.map(() => {
-          <SmallListingContainer navigateToLink={() => {}}></SmallListingContainer>;
+        {myListings.map((listingElement, index) => {
+          return (
+            <SmallListingContainer
+              key={index}
+              navigateToLink={() => {}}
+              applicationCount={listingElement.applicationCount}
+              title={listingElement.title}
+              location={listingElement.location}
+              dateEnd={listingElement.dateEnd}></SmallListingContainer>
+          );
         })}
       </div>
     </LayoutSignedInEmployer>
