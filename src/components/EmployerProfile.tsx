dffc728d14fc3f1a8ff53d2429d378ab33ceef0d
serhat <serhat@poilabs.com>
2021-01-08 03:06:06 +0300

    employer profile comp to func comp

--- src/components/EmployerProfile.tsx
+++ src/components/EmployerProfile.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import {
   Button, //H5,
   InputGroup,
@@ -7,101 +7,91 @@ import {
 } from "@blueprintjs/core";
 import styles from "../components/styles.module.css";
 
-class EmployerProfile extends React.Component<any, any> {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.inputChange = this.inputChange.bind(this);
-    this.state = {
-      title: "",
-      sector: "",
-      numOfEmployees: 0,
-      description: "",
-      address: "",
-    };
+function EmployerProfile() {
+  const [state, setState] = useState({
+    title: "",
+    address: "",
+    numOfEmployees: "",
+    description: "",
+    sector: "",
+  });
+
+  function inputChange(name, val) {
+    setState({
+      ...state,
+      [name]: val,
+    });
   }
 
-  handleClick() {
-    alert("updated" + JSON.stringify(this.state));
-  }
-  inputChange(type, e) {
-    const tmpState = {};
-    if (!e.target) {
-      tmpState[type] = e;
-      this.setState(tmpState);
-    } else {
-      tmpState[type] = e.target.value;
-      this.setState(tmpState);
-    }
+  function handleClick() {
+    alert("updated" + JSON.stringify(state));
   }
 
-  render() {
-    return (
-      <div className={styles.CompanyDetailContainer}>
-        <h2 className={[styles.titleLarge, styles.mb64].join(" ")}>Company Detail</h2>
-        <div className="flex flex-row">
-          <p className={styles.label}>
-            <b>Title</b>
-          </p>
-          <InputGroup
-            placeholder={"Type to edit"}
-            onChange={this.inputChange.bind(this, "title")}
-            defaultValue={this.state.title}
-            className={["bp3-fill", styles.inputUnderline].join(" ")}
-          />
-        </div>
-        <div className="flex flex-row">
-          <p className={styles.label}>
-            <b>Sector</b>
-          </p>
-          <InputGroup
-            placeholder={"Type to edit"}
-            onChange={this.inputChange.bind(this, "sector")}
-            defaultValue={this.state.sector}
-            className={["bp3-fill", styles.inputUnderline].join(" ")}
-          />
-        </div>
-        <div className="flex flex-row">
-          <p className={styles.label}>
-            <b>Number of employees</b>
-          </p>
-          <InputGroup
-            placeholder={"Type to edit"}
-            onChange={this.inputChange.bind(this, "numOfEmployees")}
-            defaultValue={this.state.numOfEmployees}
-            className={["bp3-fill", styles.inputUnderline].join(" ")}
-          />
-        </div>
-        <div className="flex flex-row">
-          <p className={styles.label}>
-            <b>Description</b>
-          </p>
-          <TextArea
-            placeholder={"Type to edit"}
-            onChange={this.inputChange.bind(this, "description")}
-            defaultValue={this.state.description}
-            className={["bp3-fill", styles.inputUnderline].join(" ")}
-          />
-        </div>
-        <div className="flex flex-row">
-          <p className={styles.label}>
-            <b>Address</b>
-          </p>
-          <TextArea
-            placeholder={"Type to edit"}
-            onChange={this.inputChange.bind(this, "address")}
-            defaultValue={this.state.address}
-            className={["bp3-fill", styles.inputUnderline].join(" ")}
-          />
-        </div>
-        <div className="flex justify-end">
-          <Button onClick={this.handleClick} className={["bp3-outlined", "bp3-large"].join(" ")}>
-            Update
-          </Button>
-        </div>
+  return (
+    <div className={styles.CompanyDetailContainer}>
+      <h2 className={[styles.titleLarge, styles.mb64].join(" ")}>Company Detail</h2>
+      <div className="flex flex-row">
+        <p className={styles.label}>
+          <b>Title</b>
+        </p>
+        <InputGroup
+          placeholder={"Type to edit"}
+          onChange={(text) => inputChange("title", text)}
+          defaultValue={state.title}
+          className={["bp3-fill", styles.inputUnderline].join(" ")}
+        />
       </div>
-    );
-  }
+      <div className="flex flex-row">
+        <p className={styles.label}>
+          <b>Sector</b>
+        </p>
+        <InputGroup
+          placeholder={"Type to edit"}
+          onChange={(text) => inputChange("sector", text)}
+          defaultValue={state.sector}
+          className={["bp3-fill", styles.inputUnderline].join(" ")}
+        />
+      </div>
+      <div className="flex flex-row">
+        <p className={styles.label}>
+          <b>Number of employees</b>
+        </p>
+        <InputGroup
+          placeholder={"Type to edit"}
+          onChange={(text) => inputChange("numOfEmployees", text)}
+          defaultValue={state.numOfEmployees}
+          className={["bp3-fill", styles.inputUnderline].join(" ")}
+        />
+      </div>
+      <div className="flex flex-row">
+        <p className={styles.label}>
+          <b>Description</b>
+        </p>
+        <TextArea
+          placeholder={"Type to edit"}
+          onChange={(text) => inputChange("description", text)}
+          defaultValue={state.description}
+          className={["bp3-fill", styles.inputUnderline].join(" ")}
+        />
+      </div>
+      <div className="flex flex-row">
+        <p className={styles.label}>
+          <b>Address</b>
+        </p>
+        <TextArea
+          placeholder={"Type to edit"}
+          onChange={(text) => inputChange("address", text)}
+          defaultValue={state.address}
+          className={["bp3-fill", styles.inputUnderline].join(" ")}
+        />
+      </div>
+      <div className="flex justify-end">
+        <Button onClick={() => handleClick()} className={["bp3-outlined", "bp3-large"].join(" ")}>
+          Update
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default EmployerProfile;
