ff0a94865a65b517eb5ea117e28635c56efb996c
seymurkafkas <kafkas17@itu.edu.tr>
2021-01-13 14:38:25 +0000

    Login Form isn't shown for logged in users

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import SignInForm from "../components/SignInForm";
+import { useUser } from "../services/auth/userContext";
+import * as Navigation from "../services/navigation";
+import { useRouter } from "next/router";
+
 export default function Login() {
-  return (
-    <div className="absolute w-full h-full bg-auth-background">
-      <SignInForm />
-    </div>
-  );
+  const { user, loadingUser } = useUser();
+  const router = useRouter();
+
+  if (loadingUser) {
+    return <div></div>;
+  }
+
+  if (user) {
+    Navigation.goToHome(router);
+    return <div></div>;
+  }
+  if (!loadingUser && !user) {
+    return (
+      <div className="absolute w-full h-full bg-auth-background">
+        <SignInForm />
+      </div>
+    );
+  }
 }
-//hadshbjdsajbhg
