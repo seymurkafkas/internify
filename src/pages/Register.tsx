1da47690f9bb027a79a050b12aa2c17b41426809
seymurkafkas <seymurkafkas@gmail.com>
2021-01-13 14:28:59 +0000

    Registration redirects to Home when logged in

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import SignUpForm from "../components/SignUpForm";
+import { useUser } from "../services/auth/userContext";
+import * as Navigation from "../services/navigation";
+import { useRouter } from "next/router";
 
 export default function Register() {
-  return (
-    <div className="absolute w-full h-full bg-auth-background">
-      <SignUpForm />
-    </div>
-  );
+  const { user, loadingUser } = useUser();
+  const router = useRouter();
+
+  if (loadingUser) {
+    return <div></div>;
+  }
+
+  if (user) {
+    Navigation.goToHome(router);
+    return <div></div>;
+  }
+  if (!loadingUser && !user) {
+    return (
+      <div className="absolute w-full h-full bg-auth-background">
+        <SignUpForm />
+      </div>
+    );
+  }
 }
