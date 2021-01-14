3b8e461959305f6c6ec9d18912bbbc53687c05b0
seymurkafkas <kafkas17@itu.edu.tr>
2021-01-14 00:56:42 +0000

    Searchbar redesign

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,14 +7,17 @@ function SearchBar(props: any) {
       <h2 className="text-3xl font-bold mb-9">Find Listings!</h2>
       <div className="flex space-x-4">
         <InputGroup
+          leftIcon="search"
           onChange={props.onKeywordChange}
           value={props.keyword}
           className="rounded-lg w-96"
           placeholder={"Enter a keyword"}
+          rightElement={
+            <Button icon="arrow-bottom-right" className="bp3-minimal" onClick={props.onSearchClick}>
+              Search
+            </Button>
+          }
         />
-        <Button icon="search" className="bp3-minimal" onClick={props.onSearchClick}>
-          Search
-        </Button>
       </div>
     </div>
   );
