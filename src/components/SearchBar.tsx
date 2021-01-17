3c657b74c27a4553fc63bca63945b7e8c9f8d232
Mert Buran <buranme@gmail.com>
2021-01-17 16:11:00 +0300

    designed the Search page

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,7 +4,7 @@ import { InputGroup, Button } from "@blueprintjs/core";
 function SearchBar(props: any) {
   return (
     <div className="flex flex-col space-y-2 mb-8">
-      <h2 className="text-3xl font-bold mb-9">Find Listings!</h2>
+      <h2 className="text-5xl font-bold mb-9">Find Listings</h2>
       <div className="flex space-x-4">
         <InputGroup
           leftIcon="search"
